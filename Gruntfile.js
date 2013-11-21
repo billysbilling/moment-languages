6fefb966db75ecb8e2f938c2ee50eb53bef2258a
Sebastian Seilund <sebastian@seilund.com>
2013-11-21 13:05:11 -0800

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'billy-builder': {
+            title: 'moment-languages',
+            jshint: true
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
