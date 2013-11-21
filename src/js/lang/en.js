9a02aef7060b1fb73a611fa19a5b5c5e41563a85
Sebastian Seilund <sebastian@seilund.com>
2013-11-21 13:19:12 -0800

    added code

---
+++ src/js/lang/en.js
@@ -0,0 +1,10 @@
+module.exports = {
+    monthsShort : "Jan._Feb._Mar._Apr._May_Jun._Jul._Aug._Sep._Oct._Nov._Dec.".split("_"),
+    longDateFormat: {
+        LT : "h:mm A",
+        L : "MM/DD/YYYY",
+        LL : "MMM D YYYY",
+        LLL : "MMM D YYYY LT",
+        LLLL : "dddd, MMMM Do YYYY LT"
+    }
+};
